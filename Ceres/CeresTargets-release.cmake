517b30a6bc520a192b73dcd734aa8007000b0f89
Michael Todd <eltodd@gmail.com>
2025-02-07 18:07:13 -0500

    Cont updates

--- Ceres/CeresTargets-release.cmake
+++ Ceres/CeresTargets-release.cmake
@@ -5,16 +5,6 @@
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "Ceres::ceres_cuda_kernels" for configuration "Release"
-set_property(TARGET Ceres::ceres_cuda_kernels APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(Ceres::ceres_cuda_kernels PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CUDA"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libceres_cuda_kernels.a"
-  )
-
-list(APPEND _IMPORT_CHECK_TARGETS Ceres::ceres_cuda_kernels )
-list(APPEND _IMPORT_CHECK_FILES_FOR_Ceres::ceres_cuda_kernels "${_IMPORT_PREFIX}/lib/libceres_cuda_kernels.a" )
-
 # Import target "Ceres::ceres" for configuration "Release"
 set_property(TARGET Ceres::ceres APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(Ceres::ceres PROPERTIES
